887383ec8bfca690744b8fe4361e12f6cfc413a5
KananValizada <kenan.veli@yahoo.com>
2019-12-03 11:18:10 -0800

    All done

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 let btn = document.querySelector(".header__nav-burger")
+let nav = document.querySelector(".header__nav")
 let dropDown = document.querySelector(".header__nav-menu")
 btn.addEventListener("mouseover", () => {
     dropDown.style.display = "flex"
 })
-btn.addEventListener("mouseleave", () => {
+nav.addEventListener("mouseleave", () => {
     dropDown.style.display = "none"
 })
